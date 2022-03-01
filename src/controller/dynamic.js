9be9fb0aea5dd717bcc69e5b3a0dd52c14edb8ac
unknown <montherismail90@gmail.com>
2022-03-01 18:33:48 +0200

    Create (post data to database) function relates #10

--- src/controller/dynamic.js
+++ src/controller/dynamic.js
@@ -1,5 +1,11 @@
 const getData = require('../database/quires/getData');
+const postData = require('../database/quires/postData');
 
 const getAllData = (req, res) => getData().then((result) => res.json(result.rows)).catch((err) => res.status(500).json({ massage: 'Internal Server Error', err }));
 
-module.exports = getAllData;
+const postAllData = (req, res) => {
+  const { name, age, data } = req.body;
+  postData(name, age, data).then(() => res.redirect('/'));
+};
+
+module.exports = { getAllData, postAllData };
